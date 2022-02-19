f4e505944153e58f41a18fff2f8d8f482e799f5f
dyxfigni <lack230206@gmail.com>
2022-02-19 22:17:56 +0200

    Добавил практическаую

---
+++ Dapper/Практическая 17.02 Кастумерс/Практическая 17.02 Кастумерс/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая_17._02_Кастумерс
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
